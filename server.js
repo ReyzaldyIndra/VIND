0603a929a974bdfd2a516feefce7184db160390d
potato head <Reyzaldy Indra>
2019-10-25 10:20:28 +0700

    fix(dep) server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/vind-link'));
+
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/vind-link/index.html')
+});
 
 app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
 
 console.log('Listening')
 
